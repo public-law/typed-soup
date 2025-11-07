826483ce57b93b2bbf7f578c0ff915ceb8041f28
Robert Shecter <robb@public.law>
2025-11-06 20:52:43 -0700

    remove scrapy (#6)
    * initial changes
    
    * new version

--- tests/test_typed_soup.py
+++ tests/test_typed_soup.py
@@ -1,7 +1,6 @@
 import unittest
 from bs4 import BeautifulSoup
-from scrapy.http.response.html import HtmlResponse
-from typed_soup import TypedSoup, from_response
+from typed_soup import TypedSoup
 
 
 class TestTypedSoup(unittest.TestCase):
@@ -31,17 +30,6 @@ class TestTypedSoup(unittest.TestCase):
         self.assertEqual(len(elements), 2)
         self.assertEqual(elements[0].get_text(), "First")
         self.assertEqual(elements[1].get_text(), "Second")
-
-    def test_from_response(self):
-        # Create a mock HtmlResponse
-        response = HtmlResponse(url="http://example.com",
-                                body="<div>Test</div>", encoding="utf-8")
-        typed_soup = from_response(response)
-        element = typed_soup.find("div")
-        self.assertIsNotNone(element)
-        if element:
-            self.assertEqual(element.get_text(), "Test")
-
 
 if __name__ == "__main__":
     unittest.main()
